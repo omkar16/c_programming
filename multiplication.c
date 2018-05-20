f05cfebb9103bd7756db0624434fde87eead4251
omkar16 <theomkarbhagwat@gmail.com>
2018-05-20 18:54:55 +0530

    Multiplication of two integer

---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() {
+	int a, b, c;
+	printf("Enter the first number : ");
+	scanf("%d", &a);
+	printf("Enter the second number : ");
+	scanf("%d", &b);
+	c = a * b;
+	printf("Multiplication of number a and number b is : %d \n", c);
+}
