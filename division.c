3d4e042e8241bfee33e8f6c999389a04e2854205
omkar16 <theomkarbhagwat@gmail.com>
2018-05-20 19:10:14 +0530

    Division of two numbers

---
+++ division.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() {
+	int a,b,c;
+	printf("enter the first number : ");
+	scanf("%d", &a);
+	printf("enter the second number : ");
+	scanf("%d", &b);
+	c = a/b;
+	printf("%d", c);
+}
