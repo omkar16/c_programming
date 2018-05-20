4af7b1303109db6c86ffd361e62c09b138f03a41
omkar16 <theomkarbhagwat@gmail.com>
2018-05-20 18:21:20 +0530

    Addition of two integer numbers

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Please specify first number: ");
+    scanf("%d", &a);
+    printf("Please specify second number: ");
+    scanf("%d", &b);
+    printf("%d", a + b);
+    return 0;
+}
